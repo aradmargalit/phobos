b429332f14dffdbdc58ead4345df50c3bc368afc
Arad Margalit <aradmar@gmail.com>
2020-02-09 15:22:39 -0800

    Gin installed

--- server/main.go
+++ server/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	for {
-		fmt.Println(time.Now())
-		time.Sleep(time.Second * 10)
-	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
