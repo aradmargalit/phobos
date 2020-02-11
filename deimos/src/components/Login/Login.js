0ab1bf9a2e761ca1f092025c21269b7867b59bf9
Arad Margalit <aradmar@gmail.com>
2020-02-10 21:28:01 -0800

    Make the frontend a bit prettier

--- deimos/src/components/Login/Login.js
+++ deimos/src/components/Login/Login.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button } from 'antd';
+import './Login.scss';
 
 export default function Login() {
   let BACKEND_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
   return (
-    <div>
+    <div className="button-container">
       <Button href={`${BACKEND_URL}/auth/google`}>Login with Google</Button>
-      <Button href={`${BACKEND_URL}/private/users/current`}>Current User</Button>
+      <Button href={`${BACKEND_URL}/private/users/current`}>
+        Current User
+      </Button>
+      <Button href={`${BACKEND_URL}/users/logout`}>Logout</Button>
     </div>
   );
 }
