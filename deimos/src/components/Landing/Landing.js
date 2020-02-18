fb078122786cab950ed5dadcc8892d0c238a48c8
Arad Margalit <aradmar@gmail.com>
2020-02-17 22:35:04 -0800

    Landing page looks reasonable!

--- deimos/src/components/Landing/Landing.js
+++ deimos/src/components/Landing/Landing.js
@@ -1,17 +1,36 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../contexts';
 import { Redirect } from 'react-router-dom';
-import { Spin } from 'antd';
+import { Spin, Button } from 'antd';
+import './Landing.scss';
 
 export default function Landing() {
+  const BACKEND_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
   const { user, loading } = useContext(UserContext);
   if (loading) return <Spin />;
+  if (user) return <Redirect to="/home" />;
 
-  return user ? (
-    <div>
-      <Redirect to="/home" />
+  return (
+    <div className="landing-container">
+      <h1>P H O B O S</h1>
+      <h3>a smarter fitness tracker</h3>
+      <div className="landing-container__tray">
+        <img
+          className="landing-container__hero"
+          src={require('./moon.png')}
+          alt="rocket"
+        />
+        <div className="landing-container__actions">
+          <Button href={`${BACKEND_URL}/auth/google`} className="ant-btn big-button" icon="google" type="primary">
+            Sign Up with Google
+          </Button>
+          <i>already have an account?</i>
+          <Button href={`${BACKEND_URL}/auth/google`} className="ant-btn small-button" icon="login" ghost type="primary">
+            Sign In
+          </Button>
+        </div>
+      </div>
     </div>
-  ) : (
-    <h1>Oh, you're not logged in</h1>
   );
 }
