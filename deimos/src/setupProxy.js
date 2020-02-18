feccc53fd5841b44f2ed46b91bedb1a047d23692
Arad Margalit <aradmar@gmail.com>
2020-02-18 00:09:53 -0800

    ESLint rules enforced

--- deimos/src/setupProxy.js
+++ deimos/src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require('http-proxy-middleware');
+
 const BACKEND_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
-module.exports = function(app) {
+module.exports = (app) => {
   app.use(proxy('/auth/google', { target: BACKEND_URL }));
   app.use(proxy('/private/users/current', { target: BACKEND_URL }));
 };
