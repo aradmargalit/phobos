75a64fe85451e463be6e81d040ecc26fbc592b1a
Arad Margalit <aradmar@gmail.com>
2020-03-08 16:59:14 -0700

    Hook up trendline

--- deimos/src/components/Statistics/Statistics.jsx
+++ deimos/src/components/Statistics/Statistics.jsx
@@ -9,34 +9,16 @@ import {
 
 import { fetchStatistics } from '../../apis/phobos-api';
 
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-];
+const makeGraphData = (arr) => arr.map((datum, idx) => ({ idx, datum }));
 
 export default function Statistics() {
   const [loading, setLoading] = useState(true);
-  const [stats, setStats] = useState({ workouts: 0, hours: 0, miles: 0 });
-  const { workouts, hours, miles } = stats;
+  const [stats, setStats] = useState({
+    workouts: 0, hours: 0, miles: 0, last_ten: [],
+  });
+  const {
+    workouts, hours, miles, last_ten: lastTen,
+  } = stats;
 
   useEffect(() => {
     fetchStatistics(setStats, setLoading);
@@ -77,8 +59,9 @@ export default function Statistics() {
             value={miles.toFixed(2)}
           />
         </div>
-        <LineChart width={300} height={100} data={data}>
-          <Line type="monotone" dataKey="pv" stroke="#0e5a6d" strokeWidth={2} dot={false} />
+        <h3>Last 10 Days</h3>
+        <LineChart width={300} height={100} data={makeGraphData(lastTen)}>
+          <Line type="monotone" dataKey="datum" stroke="#0e5a6d" strokeWidth={2} dot={false} />
         </LineChart>
         <Button href="http://localhost:3000/graph">More Graph?</Button>
       </div>
