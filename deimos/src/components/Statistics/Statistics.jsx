4087ab69e5499cc7571876e262454bd814b163f8
Arad Margalit <aradmar@gmail.com>
2020-03-10 21:49:48 -0700

    Progress on Pie

--- deimos/src/components/Statistics/Statistics.jsx
+++ deimos/src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import './Statistics.scss';
 
 import {
@@ -7,23 +8,32 @@ import {
 } from '@ant-design/icons';
 import { Button, Spin, Statistic } from 'antd';
 import React, { useContext, useEffect } from 'react';
-import { Line, LineChart } from 'recharts';
+import { Cell, Line, LineChart, Pie, PieChart } from 'recharts';
 
 import { fetchStatistics } from '../../apis/phobos-api';
 import { StatsContext } from '../../contexts';
 
 const makeGraphData = arr => arr.map((datum, idx) => ({ idx, datum }));
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const renderLabel = d => d.name;
+
 export default function Statistics({ loading, setLoading }) {
   const { stats, setStats } = useContext(StatsContext);
-  const { workouts, hours, miles, last_ten: lastTen } = stats;
+  const {
+    workouts,
+    hours,
+    miles,
+    last_ten: lastTen,
+    type_breakdown: typeBreakdown,
+  } = stats;
 
-  useEffect(
-    () => {
-      fetchStatistics(setStats, setLoading);
-    },
-    [setStats, setLoading]
-  );
+  useEffect(() => {
+    fetchStatistics(setStats, setLoading);
+  }, [setStats, setLoading]);
+
+  if (loading) return <Spin />;
 
   return (
     <Spin spinning={loading}>
@@ -53,18 +63,39 @@ export default function Statistics({ loading, setLoading }) {
             }
             value={miles.toFixed(2)}
           />
+          <div className="statistics--trendline">
+            <h3>Last 10 Days</h3>
+            <LineChart width={250} height={70} data={makeGraphData(lastTen)}>
+              <Line
+                type="monotone"
+                dataKey="datum"
+                stroke="#0e5a6d"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+            <Button href="http://localhost:3000/graph">More Graph?</Button>
+          </div>
         </div>
-        <h3>Last 10 Days</h3>
-        <LineChart width={300} height={100} data={makeGraphData(lastTen)}>
-          <Line
-            type="monotone"
-            dataKey="datum"
-            stroke="#0e5a6d"
-            strokeWidth={2}
-            dot={false}
-          />
-        </LineChart>
-        <Button href="http://localhost:3000/graph">More Graph?</Button>
+
+        <div className="statistics--pie">
+          <PieChart width={500} height={300}>
+            <Pie
+              data={typeBreakdown}
+              innerRadius={80}
+              outerRadius={100}
+              fill="#8884d8"
+              paddingAngle={3}
+              dataKey="portion"
+              nameKey="name"
+              label={renderLabel}
+            >
+              {typeBreakdown.map((entry, index) => (
+                <Cell key={entry.name} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
       </div>
     </Spin>
   );
--- server/controllers/statistics.go
+++ server/controllers/statistics.go
@@ -9,6 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type typePortion struct {
+	Name    string `json:"name"`
+	Portion int    `json:"portion"`
+}
+
 // GetUserStatistics returns some fun user statistics for the frontend
 func (e *Env) GetUserStatistics(c *gin.Context) {
 	// Pull user out of context to figure out which activities to grab
@@ -22,19 +27,26 @@ func (e *Env) GetUserStatistics(c *gin.Context) {
 		panic(err)
 	}
 
+	at, err := e.DB.GetActivityTypes()
+	if err != nil {
+		panic(err)
+	}
+
 	// Now that we have activities, let's cronch the numbies
 	totalWorkouts := len(a)
 	totalHours := calculateTotalHours(a)
 	totalMiles := calculateMileage(a)
 	lastTen := calculateLastTenDays(a)
+	typeBreakdown := calculateTypeBreakdown(a, at)
 
 	response := struct {
-		Workouts int       `json:"workouts"`
-		Hours    float64   `json:"hours"`
-		Miles    float64   `json:"miles"`
-		LastTen  []float64 `json:"last_ten"`
+		Workouts      int           `json:"workouts"`
+		Hours         float64       `json:"hours"`
+		Miles         float64       `json:"miles"`
+		LastTen       []float64     `json:"last_ten"`
+		TypeBreakdown []typePortion `json:"type_breakdown"`
 	}{
-		totalWorkouts, totalHours, totalMiles, lastTen,
+		totalWorkouts, totalHours, totalMiles, lastTen, typeBreakdown,
 	}
 
 	c.JSON(http.StatusOK, response)
@@ -82,6 +94,31 @@ func calculateLastTenDays(activities []models.Activity) (lastTen []float64) {
 
 		// Append the sum to the slice
 		lastTen = append(lastTen, running)
+	}
+	return
+}
+
+func calculateTypeBreakdown(activities []models.Activity, activityTypes []models.ActivityType) (typePortions []typePortion) {
+	typeMap := make(map[int]int)
+	for _, activity := range activities {
+		running, ok := typeMap[activity.ActivityTypeID]
+		if !ok {
+			running = 0
+		}
+
+		running++
+		typeMap[activity.ActivityTypeID] = running
+	}
+
+	// Build a quick map of activity type ids to names
+	activityTypeMap := map[int]string{}
+	for _, aT := range activityTypes {
+		activityTypeMap[aT.ID] = aT.Name
+	}
+
+	// Now, calculate proportions
+	for typeID, count := range typeMap {
+		typePortions = append(typePortions, typePortion{activityTypeMap[typeID], count})
 	}
 	return
 }
