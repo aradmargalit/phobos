d96a271f5d352df02028778fc95a94c0c378b7b8
Arad Margalit <aradmar@gmail.com>
2020-03-15 17:25:39 -0700

    Simplify home page

--- deimos/src/components/Statistics/Statistics.jsx
+++ deimos/src/components/Statistics/Statistics.jsx
@@ -10,28 +10,12 @@ import {
 import { Spin, Statistic } from 'antd';
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  Cell,
-  Line,
-  LineChart,
-  Pie,
-  PieChart,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
+import { Line, LineChart, ResponsiveContainer } from 'recharts';
 
 import { fetchStatistics } from '../../apis/phobos-api';
 import { StatsContext } from '../../contexts';
 
 const makeGraphData = arr => arr.map((datum, idx) => ({ idx, datum }));
-
-const COLORS = ['#bd4946', '#d4524e', '#ec5b57', '#f07c79', '#f49d9a'];
-
-const renderLabel = d => d.name;
 
 export default function Statistics({ loading, setLoading }) {
   const { stats, setStats } = useContext(StatsContext);
@@ -40,8 +24,8 @@ export default function Statistics({ loading, setLoading }) {
     hours,
     miles,
     last_ten: lastTen,
-    type_breakdown: typeBreakdown,
-    day_breakdown: dayBreakdown,
+    // type_breakdown: typeBreakdown,
+    // day_breakdown: dayBreakdown,
   } = stats;
 
   useEffect(() => {
@@ -52,8 +36,8 @@ export default function Statistics({ loading, setLoading }) {
 
   return (
     <Spin spinning={loading}>
-      <div className="statistics">
-        <div className="stats">
+      <div className="stats">
+        <div className="statistics-grid">
           <Statistic
             title={
               <span>
@@ -78,7 +62,33 @@ export default function Statistics({ loading, setLoading }) {
             }
             value={miles.toFixed(2)}
           />
-          <div className="statistics--dow">
+        </div>
+        <div className="statistics--trendline">
+          <h3>Last 10 Days</h3>
+          <ResponsiveContainer id="trendline-wrapper" width="75%">
+            <LineChart data={makeGraphData(lastTen)}>
+              <Line
+                type="monotone"
+                dataKey="datum"
+                stroke="#0e5a6d"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+          <Link to="/graph">More Graph </Link>.
+        </div>
+      </div>
+    </Spin>
+  );
+}
+
+// TODO: Find a home for these
+// const COLORS = ['#bd4946', '#d4524e', '#ec5b57', '#f07c79', '#f49d9a'];
+
+// const renderLabel = d => d.name;
+
+/* <div className="statistics--dow">
             <h3>Daily Breakdown</h3>
             <BarChart
               width={250}
@@ -105,20 +115,6 @@ export default function Statistics({ loading, setLoading }) {
               </Bar>
             </BarChart>
           </div>
-          <div className="statistics--trendline">
-            <h3>Last 10 Days</h3>
-            <LineChart width={250} height={65} data={makeGraphData(lastTen)}>
-              <Line
-                type="monotone"
-                dataKey="datum"
-                stroke="#0e5a6d"
-                strokeWidth={2}
-                dot={false}
-              />
-            </LineChart>
-            <Link to="/graph">More Graph </Link>.
-          </div>
-
           <div className="statistics--pie">
             <PieChart width={400} height={200}>
               <Pie
@@ -136,9 +132,4 @@ export default function Statistics({ loading, setLoading }) {
                 ))}
               </Pie>
             </PieChart>
-          </div>
-        </div>
-      </div>
-    </Spin>
-  );
-}
+                </div> */
