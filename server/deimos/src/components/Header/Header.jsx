dd080e7ad9556e4f9e1faa28b3e7b20a2afa3520
Arad Margalit <aradmar@gmail.com>
2020-04-04 18:21:40 -0700

    First working pass at a stateful button

--- server/deimos/src/components/Header/Header.jsx
+++ server/deimos/src/components/Header/Header.jsx
@@ -1,28 +1,44 @@
 import './Header.scss';
 
+import { CheckCircleOutlined } from '@ant-design/icons';
 import { Button, PageHeader } from 'antd';
-import React from 'react';
+import React, { useContext } from 'react';
 import { withRouter } from 'react-router-dom';
 
+import { UserContext } from '../../contexts';
 import IdentityButton from '../IdentityButton';
 
-const extra = () => (
+const extra = (registeredWithStrava, loading) => (
   <div className="header__extra">
-    <Button className="header__strava-button" href="/strava/auth">
-      Connect with Strava
+    <Button
+      disabled={loading || registeredWithStrava}
+      className={`header__strava-button${
+        registeredWithStrava ? '--registered' : ''
+      }`}
+      href="/strava/auth"
+    >
+      {registeredWithStrava ? 'Connected with Strava' : 'Connect with Strava'}
+      {registeredWithStrava && <CheckCircleOutlined />}
     </Button>
     <IdentityButton />
   </div>
 );
 
 const Header = ({ history, showBack, showStrava }) => {
+  const { user } = useContext(UserContext);
   return (
     <PageHeader
       className="header"
       onBack={showBack ? () => history.push('/home') : null}
       title="PHOBOS"
       subTitle="A Fitness Tracker"
-      extra={showStrava ? extra() : <IdentityButton />}
+      extra={
+        showStrava ? (
+          extra(user.payload && user.payload.strava_token, user.loading)
+        ) : (
+          <IdentityButton />
+        )
+      }
     />
   );
 };
---
+++ server/response_types/user_response.go
@@ -0,0 +1,12 @@
+package responsetypes
+
+// UserResponse represents the database user
+type UserResponse struct {
+	ID          int    `json:"id" db:"id"`
+	Name        string `json:"name "db:"name"`
+	GivenName   string `json:"given_name" db:"given_name"`
+	Email       string `json:"email" db:"email"`
+	StravaToken bool   `json:"strava_token" db:"strava_token"`
+	CreatedAt   string `json:"created_at" db:"created_at"`
+	UpdatedAt   string `json:"updated_at" db:"updated_at"`
+}
