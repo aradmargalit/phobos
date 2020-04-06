1143230968b3b3dbbc7cf3054c758fd24ed3ad40
Arad Margalit <aradmar@gmail.com>
2020-04-05 18:42:29 -0700

    Display greater than day-long

--- server/deimos/src/utils/dataFormatUtils.js
+++ server/deimos/src/utils/dataFormatUtils.js
@@ -1,15 +1,21 @@
 import moment from 'moment';
 
 const dateFormat = 'MMMM Do, YYYY';
+const minutesInDay = 24 * 60;
 
 export const minutesToHMS = minutes => {
   if (!minutes || minutes === 0 || !Number.isFinite(minutes)) {
     return '-';
   }
-  return moment()
+
+  const hms = moment()
     .startOf('day')
     .add(minutes, 'minutes')
     .format('HH:mm:ss');
+
+  return `${
+    minutes >= minutesInDay ? `${Math.floor(minutes / minutesInDay)}d, ` : ''
+  }${hms}`;
 };
 
 export const formatDate = date => moment(date).format(dateFormat);
