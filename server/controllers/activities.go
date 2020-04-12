1a556602688a57e23cf23134e2ed05210d9633aa
Arad Margalit <aradmar@gmail.com>
2020-04-12 11:01:43 -0700

    Cleaner monthly controller

--- server/controllers/activities.go
+++ server/controllers/activities.go
@@ -122,17 +122,19 @@ func (e *Env) GetMonthlySums(c *gin.Context) {
 		return
 	}
 
-	// In order to figure out which days have no activities, we start with an array with every day from
-	// their first activity until now
-	firstActivityDate, _ := time.Parse("2006-01-02 15:04:05", a[len(a)-1].ActivityDate)
+	months := bucketIntoMonths(a)
 
-	// Make a map of days to whether or not they were skipped
-	skippedMap := map[time.Time]bool{}
+	c1 := make(chan map[string]float64, 1)
+	c2 := make(chan map[string]float64, 1)
+	c3 := make(chan map[string]float64, 1)
 
-	// Start by assuming each date is skipped, we'll invalidate that assumption as we go
-	for d := firstActivityDate; !utils.DateEqual(d, time.Now().AddDate(0, 0, 1)); d = d.AddDate(0, 0, 1) {
-		skippedMap[utils.RoundTimeToDay(d)] = true
-	}
+	go makeDurationMap(a, months, c1)
+	go makeDistanceMap(a, months, c2)
+	go makeSkippedMap(a, months, c3)
+
+	durationMap := <-c1
+	distanceMap := <-c2
+	skippedMap := <-c3
 
 	/* This is in the format:
 	{
@@ -143,55 +145,109 @@ func (e *Env) GetMonthlySums(c *gin.Context) {
 		}
 	}
 	*/
-	monthMap := map[string]map[string]float64{}
 
-	for _, activity := range a {
-		m, _ := time.Parse("2006-01-02 15:04:05", activity.ActivityDate)
+	response := []monthlySum{}
+	for _, month := range months {
+		mSum := monthlySum{Month: month, Duration: durationMap[month], Miles: distanceMap[month], DaysSkipped: skippedMap[month]}
+		response = append(response, mSum)
+	}
 
-		// Go into our date map and mark the date as unskipped
-		skippedMap[utils.RoundTimeToDay(m)] = false
+	c.JSON(http.StatusOK, response)
+}
+
+type monthlySum struct {
+	Month       string  `json:"month"`
+	Duration    float64 `json:"duration"`
+	Miles       float64 `json:"miles"`
+	DaysSkipped float64 `json:"days_skipped"`
+}
+
+func bucketIntoMonths(activities []responsetypes.ActivityResponse) []string {
+	months := []string{}
+	var previousMonth string
+	for _, a := range activities {
+		t, _ := time.Parse("2006-01-02 15:04:05", a.ActivityDate)
 
 		// Format is "January 2020"
-		month := fmt.Sprintf("%v %v", m.Month(), m.Year())
-		_, ok := monthMap[month]
-
-		// If !ok, we've never seen this month before, so initialize it to 0s
-		if !ok {
-			monthMap[month] = map[string]float64{"duration": 0, "miles": 0, "days_skipped": 0}
+		month := fmt.Sprintf("%v %v", t.Month(), t.Year())
+		if month != previousMonth {
+			months = append(months, month)
+			previousMonth = month
 		}
 
-		// Otherwise, add duration, and distance (if mileage)
-		monthMap[month]["duration"] += activity.Duration
-		if activity.Unit == "miles" {
-			monthMap[month]["miles"] += activity.Distance
+	}
+	return months
+}
+
+func makeDurationMap(activities []responsetypes.ActivityResponse, months []string, c chan map[string]float64) {
+	durationMap := map[string]float64{}
+	for _, month := range months {
+		durationMap[month] = 0
+	}
+
+	for _, a := range activities {
+		t, _ := time.Parse("2006-01-02 15:04:05", a.ActivityDate)
+
+		// Format is "January 2020"
+		month := fmt.Sprintf("%v %v", t.Month(), t.Year())
+		durationMap[month] += a.Duration
+	}
+	c <- durationMap
+}
+
+func makeDistanceMap(activities []responsetypes.ActivityResponse, months []string, c chan map[string]float64) {
+	distanceMap := map[string]float64{}
+	for _, month := range months {
+		distanceMap[month] = 0
+	}
+	for _, a := range activities {
+		t, _ := time.Parse("2006-01-02 15:04:05", a.ActivityDate)
+
+		// Format is "January 2020"
+		month := fmt.Sprintf("%v %v", t.Month(), t.Year())
+		if a.Unit == "miles" {
+			distanceMap[month] += a.Distance
 		}
+	}
+	c <- distanceMap
+}
+
+func makeSkippedMap(activities []responsetypes.ActivityResponse, months []string, c chan map[string]float64) {
+	// In order to figure out which days have no activities, we start with an array with every day from
+	// their first activity until now
+	firstActivityDate, _ := time.Parse("2006-01-02 15:04:05", activities[len(activities)-1].ActivityDate)
+
+	// Make a map of days to whether or not they were skipped
+	skippedMap := map[time.Time]bool{}
+
+	// Start by assuming each date is skipped, we'll invalidate that assumption as we go
+	for d := firstActivityDate; !utils.DateEqual(d, time.Now().AddDate(0, 0, 1)); d = d.AddDate(0, 0, 1) {
+		skippedMap[utils.RoundTimeToDay(d)] = true
+	}
+
+	for _, a := range activities {
+		t, _ := time.Parse("2006-01-02 15:04:05", a.ActivityDate)
+
+		// Go into our date map and mark the date as unskipped
+		skippedMap[utils.RoundTimeToDay(t)] = false
 	}
 
 	// After going through every activity, we need to summarize the skipped days
-	for month, payload := range monthMap {
+	monthlySkips := map[string]float64{}
+
+	for _, month := range months {
+		monthlySkips[month] = 0
 		m := strings.Split(month, " ")[0]
 		y := strings.Split(month, " ")[1]
 
 		// For each skipped activity, see if it matches, and if so, add it to the tally
 		for sA, wasSkipped := range skippedMap {
 			if wasSkipped && sA.Month().String() == m && strconv.Itoa(sA.Year()) == y {
-				newPayload := payload
-				newPayload["days_skipped"]++
-				monthMap[month] = newPayload
+
+				monthlySkips[month]++
 			}
 		}
 	}
 
-	type monthlySum struct {
-		Month       string  `json:"month"`
-		Duration    float64 `json:"duration"`
-		Miles       float64 `json:"miles"`
-		DaysSkipped float64 `json:"days_skipped"`
-	}
-
-	response := []monthlySum{}
-	for k, v := range monthMap {
-		response = append(response, monthlySum{Month: k, Duration: v["duration"], Miles: v["miles"], DaysSkipped: v["days_skipped"]})
-	}
-	c.JSON(http.StatusOK, response)
+	c <- monthlySkips
 }
