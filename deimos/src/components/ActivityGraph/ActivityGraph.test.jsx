b2d546d0dd2f27b22eefee696b32a1f8bd328f29
Arad Margalit <aradmar@gmail.com>
2020-04-13 20:41:23 -0700

    Basic wrappper test

--- deimos/src/components/ActivityGraph/ActivityGraph.test.jsx
+++ deimos/src/components/ActivityGraph/ActivityGraph.test.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import ActivityGraph from './ActivityGraph';
 
 const generateComponent = props => {
-  const defaultProps = {};
+  const defaultProps = {
+    monthlyData: [{ month: 'January 2020', duration: 123 }],
+  };
 
   const mergedProps = { ...defaultProps, ...props };
 
@@ -13,8 +15,23 @@ const generateComponent = props => {
 
 describe('<ActivityGraph />', () => {
   it('renders without crashing', () => {
-    generateComponent({
-      monthlyData: [{ month: 'January 2020', duration: 123 }],
+    generateComponent();
+  });
+
+  describe('monthly average', () => {
+    it('calculates the correct average with one month', () => {
+      const component = generateComponent();
+      expect(component.find('ReferenceLine').prop('y')).toEqual(123 / 60);
+    });
+
+    it('calculates the correct average with multiple months', () => {
+      const component = generateComponent({
+        monthlyData: [
+          { month: 'January 2020', duration: 60 },
+          { month: 'February 2020', duration: 180 },
+        ],
+      });
+      expect(component.find('ReferenceLine').prop('y')).toEqual(2);
     });
   });
 });
