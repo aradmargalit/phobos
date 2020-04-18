f2395af0e14aceb660127de905f771e43c02b97c
Arad Margalit <aradmar@gmail.com>
2020-04-18 16:14:38 -0700

    Remove xdescribe

--- deimos/src/components/IntervalGraph/IntervalGraph.test.jsx
+++ deimos/src/components/IntervalGraph/IntervalGraph.test.jsx
@@ -19,21 +19,4 @@ describe('<IntervalGraph />', () => {
   it('renders', () => {
     generateComponent();
   });
-
-  xdescribe('monthly average', () => {
-    it('calculates the correct average with one month', () => {
-      const component = generateComponent();
-      expect(component.find('ReferenceLine').prop('y')).toEqual(123 / 60);
-    });
-
-    it('calculates the correct average with multiple months', () => {
-      const component = generateComponent({
-        monthlyData: [
-          { month: 'January 2020', duration: 60 },
-          { month: 'February 2020', duration: 180 },
-        ],
-      });
-      expect(component.find('ReferenceLine').prop('y')).toEqual(2);
-    });
-  });
 });
