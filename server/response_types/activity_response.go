b41d9ed450930e740f00c6511ee29639d2b5dfec
Arad Margalit <aradmar@gmail.com>
2020-04-26 18:54:53 -0700

    Utils tests are done

--- server/response_types/activity_response.go
+++ server/response_types/activity_response.go
@@ -8,7 +8,7 @@ type ActivityResponse struct {
 	Name           string               `json:"name" db:"name"`
 	ActivityDate   string               `json:"activity_date" db:"activity_date"`
 	ActivityTypeID int                  `json:"activity_type_id" db:"activity_type_id"`
-	ActivityType   activityTypeResponse `json:"activity_type" db:"activity_type"`
+	ActivityType   ActivityTypeResponse `json:"activity_type" db:"activity_type"`
 	OwnerID        int                  `json:"owner_id" db:"owner_id"`
 	Duration       float64              `json:"duration" db:"duration"`
 	Distance       float64              `json:"distance" db:"distance"`
@@ -20,7 +20,7 @@ type ActivityResponse struct {
 	UpdatedAt      string               `json:"updated_at" db:"updated_at"`
 }
 
-type activityTypeResponse struct {
+type ActivityTypeResponse struct {
 	ID   int    `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
 }
--- server/utils/statistics_utils_test.go
+++ server/utils/statistics_utils_test.go
@@ -11,14 +11,14 @@ import (
 
 func getActivityResponses(n int, dateStepHours int) []responsetypes.ActivityResponse {
 	activities := []responsetypes.ActivityResponse{}
-	startDate, _ := time.Parse(layout, "2001-01-02 03:04:05")
+	startDate, _ := time.Parse(dbLayout, "2001-01-02 03:04:05")
 
 	for i := 1; i <= n; i++ {
 		step, _ := time.ParseDuration(fmt.Sprintf("%vh", dateStepHours*i))
 		activities = append(activities, responsetypes.ActivityResponse{
 			ID:           i,
 			Name:         fmt.Sprintf("Activity Number: %v", i),
-			ActivityDate: startDate.Add(step).String(),
+			ActivityDate: startDate.Add(step).Format(dbLayout),
 			Duration:     1,
 			Unit:         "miles",
 			Distance:     1,
@@ -49,8 +49,50 @@ func TestCalculateMileage(t *testing.T) {
 }
 
 func TestCalculateLastTenDays(t *testing.T) {
-	// Create 20 activities across 20 days
 	activities := []responsetypes.ActivityResponse{{Duration: 10, ActivityDate: time.Now().UTC().Format(dbLayout)}}
 	want := []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 10}
 	assert.Equal(t, want, CalculateLastTenDays(activities, 0))
+}
+
+func TestCalculateTypeBreakdown(t *testing.T) {
+	activities := []responsetypes.ActivityResponse{
+		{ActivityType: responsetypes.ActivityTypeResponse{Name: "Run"}},
+		{ActivityType: responsetypes.ActivityTypeResponse{Name: "Run"}},
+		{ActivityType: responsetypes.ActivityTypeResponse{Name: "Run"}},
+		{ActivityType: responsetypes.ActivityTypeResponse{Name: "Swim"}},
+	}
+
+	portions := CalculateTypeBreakdown(activities)
+	assert.Contains(t, portions, TypePortion{Name: "Run", Portion: 3})
+	assert.Contains(t, portions, TypePortion{Name: "Other", Portion: 0})
+}
+
+func TestCalculateTypeBreakdownWithOtherActivities(t *testing.T) {
+	activities := []responsetypes.ActivityResponse{}
+	for i := 0; i < 100; i++ {
+		activities = append(activities, responsetypes.ActivityResponse{ActivityType: responsetypes.ActivityTypeResponse{Name: "Run"}})
+	}
+	activities = append(activities, responsetypes.ActivityResponse{ActivityType: responsetypes.ActivityTypeResponse{Name: "Jog"}})
+	activities = append(activities, responsetypes.ActivityResponse{ActivityType: responsetypes.ActivityTypeResponse{Name: "Swim"}})
+
+	portions := CalculateTypeBreakdown(activities)
+	assert.Contains(t, portions, TypePortion{Name: "Run", Portion: 100})
+	assert.Contains(t, portions, TypePortion{Name: "Other", Portion: 2})
+	assert.NotContains(t, portions, TypePortion{Name: "Jog", Portion: 1})
+}
+
+func TestCalculateDayBreakdown(t *testing.T) {
+	// Create 20 1 mile runs
+	activities := getActivityResponses(20, 24)
+	want := []DayBreakdown([]DayBreakdown{
+		{DOW: "Monday", Count: 3},
+		{DOW: "Tuesday", Count: 2},
+		{DOW: "Wednesday", Count: 3},
+		{DOW: "Thursday", Count: 3},
+		{DOW: "Friday", Count: 3},
+		{DOW: "Saturday", Count: 3},
+		{DOW: "Sunday", Count: 3},
+	})
+
+	assert.Equal(t, want, CalculateDayBreakdown(activities))
 }
