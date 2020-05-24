59eae7efe9e25d75bc668f3338da87383d33258c
Arad Margalit <aradmar@gmail.com>
2020-05-24 16:23:49 -0700

    Basic cleanup and refactor as prep work

---
+++ deimos/src/components/Trendline/Trendline.jsx
@@ -0,0 +1,23 @@
+import './Trendline.scss';
+
+import { Tooltip } from 'antd';
+import React from 'react';
+import { Line, LineChart, ResponsiveContainer } from 'recharts';
+
+const makeGraphData = arr => arr.map((datum, idx) => ({ idx, datum }));
+
+export default function Trendline({ trendData }) {
+  console.log(trendData);
+  return (
+    <div className="trendline">
+      <Tooltip title="Each day's workout total minutes for the past 10 days.">
+        <h3>Last 10 Days</h3>
+      </Tooltip>
+      <ResponsiveContainer id="trendline-wrapper" width="100%">
+        <LineChart data={makeGraphData(trendData)}>
+          <Line type="monotone" dataKey="datum" stroke="#0e5a6d" strokeWidth={2} dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
