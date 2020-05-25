11a8d8d8d1be34552c0eb8c84022dc44bde02e41
Arad Margalit <aradmar@gmail.com>
2020-05-25 09:52:27 -0700

    Add safety mechanism to 1-point trends

--- deimos/src/components/Trendline/Trendline.jsx
+++ deimos/src/components/Trendline/Trendline.jsx
@@ -29,22 +29,21 @@ export default function Trendline({ activityTimtestamp }) {
         <Select defaultValue="l10" onChange={v => setLookback(v)}>
           <Option value="l10">Last 10 Days</Option>
           <Option value="l7">Last 7 Days</Option>
-          {/* Disable if it's the first day, since a single point isn't a trend */}
-          <Option value="lw" disabled={new Date().getDay() === 1}>
-            This Week
-          </Option>
-          <Option value="lm" disabled={new Date().getDate() === 1}>
-            This Month
-          </Option>
+          <Option value="lw">This Week</Option>
+          <Option value="lm">This Month</Option>
         </Select>
         <CountUp end={sumHours(trendPoints.payload)} decimals={2} decimal="." duration={2.5} />
         <h3>hours total</h3>
       </div>
-      <ResponsiveContainer id="trendline-wrapper" width="100%">
-        <LineChart data={makeGraphData(trendPoints.payload)}>
-          <Line type="monotone" dataKey="datum" stroke="#0e5a6d" strokeWidth={2} dot={false} />
-        </LineChart>
-      </ResponsiveContainer>
+      {trendPoints.payload.length > 1 ? (
+        <ResponsiveContainer id="trendline-wrapper" width="100%">
+          <LineChart data={makeGraphData(trendPoints.payload)}>
+            <Line type="monotone" dataKey="datum" stroke="#0e5a6d" strokeWidth={2} dot={false} />
+          </LineChart>
+        </ResponsiveContainer>
+      ) : (
+        <h3 style={{ marginTop: '30px' }}>There are too few data points to make a trend!</h3>
+      )}
     </div>
   );
 }
