249c1ee093553538284cddfb10fe23d8ceabbe21
Arad Margalit <aradmar@gmail.com>
2020-06-14 19:31:04 -0700

    DOWBarChart tests

---
+++ deimos/src/components/DOWBarChart/DOWBarChart.test.jsx
@@ -0,0 +1,19 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import DOWBarChart from './DOWBarChart';
+
+const generateComponent = props => {
+  const defaultProps = {
+    dayBreakdown: [{ day_of_week: 'Monday', count: 10 }],
+  };
+  const mergedProps = { ...defaultProps, ...props };
+
+  return shallow(<DOWBarChart {...mergedProps} />);
+};
+
+describe('<DOWBarChart />', () => {
+  it('renders correctly', () => {
+    expect(generateComponent()).toMatchSnapshot();
+  });
+});
