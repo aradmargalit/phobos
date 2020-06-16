3ddfeee406b90e984452158ecafb99db1ee4f74c
Arad Margalit <aradmargalit@users.noreply.github.com>
2020-06-15 20:40:22 -0700

    Map activity types (#41)
    * Adds activity type map for bettter matching with strava
    
    * update githooks

--- server/internal/models/activity_type.go
+++ server/internal/models/activity_type.go
@@ -2,8 +2,9 @@ package models
 
 // ActivityType represents a type for a workout
 type ActivityType struct {
-	ID        int    `json:"id" db:"id"`
-	Name      string `json:"name" db:"name"`
-	CreatedAt string `json:"created_at" db:"created_at"`
-	UpdatedAt string `json:"updated_at" db:"updated_at"`
+	ID         int    `json:"id" db:"id"`
+	Name       string `json:"name" db:"name"`
+	StravaName string `json:"strava_name" db:"strava_name"`
+	CreatedAt  string `json:"created_at" db:"created_at"`
+	UpdatedAt  string `json:"updated_at" db:"updated_at"`
 }
