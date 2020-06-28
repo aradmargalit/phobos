b86d4cfc780cac90ebec0719ead8cfa5054cdead
Arad Margalit <aradmar@gmail.com>
2020-06-28 08:36:14 -0700

    First workable UI for setting goals

--- deimos/src/components/GoalsModal/GoalsModal.jsx
+++ deimos/src/components/GoalsModal/GoalsModal.jsx
@@ -1,8 +1,14 @@
 import './GoalsModal.scss';
 
 import { AimOutlined } from '@ant-design/icons';
-import { InputNumber, Modal, notification } from 'antd';
-import React, { useState } from 'react';
+import { Form, InputNumber, Modal } from 'antd';
+import React from 'react';
+
+const unitSizeMap = {
+  Week: 7,
+  Month: 31, // Not always true, but good enough,
+  Year: 365,
+};
 
 const iconTitle = (text, icon) => (
   <span>
@@ -11,33 +17,65 @@ const iconTitle = (text, icon) => (
   </span>
 );
 
+// Is this a bad function name? Maybe.
+const getMaxBy = (metric, unit) => {
+  // If it's a percentage, it's always capped at 100%, easy!
+  if (metric.includes('%')) {
+    return 100;
+  }
+
+  if (metric.toLowerCase() === 'mile') {
+    return unitSizeMap[unit] * 200; // Assume nobody is running or biking >= 200 miles per day
+  }
+
+  // Hours
+  return unitSizeMap[unit] * 24; // 24 hours per day
+};
+
+const { Item } = Form;
+
+// TODO convert to form, I want validation + reset + onSubmit func
 export default function GoalsModal({ visible, onCancel, unit, metricName }) {
-  const [goal, setGoal] = useState(null);
+  const [form] = Form.useForm();
 
-  const handleSubmit = async () => {
-    if (!goal || goal < 1) {
-      notification.error({
-        message: 'Goal must be > 0. Please cancel if you do not wish to set a goal.',
-      });
-      return;
-    }
-
-    // Close the modal
+  const handleCancel = () => {
+    form.resetFields();
     onCancel();
   };
 
+  // eslint-disable-next-line no-unused-vars
+  const onFinish = values => {
+    // Do something with values
+    handleCancel();
+  };
+
   return (
-    <Modal className="goals-modal" visible={visible} onCancel={onCancel} onOk={handleSubmit}>
+    <Modal
+      className="goals-modal"
+      width={500}
+      visible={visible}
+      onCancel={handleCancel}
+      onOk={() => form.submit()}
+      destroyOnClose
+    >
       <h3>
         {iconTitle(
           `Set a ${unit}ly ${metricName} goal...`,
           <AimOutlined style={{ marginRight: '10px' }} />
         )}
       </h3>
-      <div className="goal-entry">
-        <InputNumber value={goal} onChange={v => setGoal(v)} placeholder={2} min={0} />
-        <h4>{metricName}</h4>
-      </div>
+      <h5>{`This value will persist across ${unit.toLowerCase()}s until you clear it.`}</h5>
+      <Form form={form} onFinish={onFinish}>
+        <Item
+          label={`New Goal (${metricName})`}
+          name="goal"
+          nostyle
+          rules={[{ required: true, message: 'You must save a goal or press "Cancel"' }]}
+        >
+          <InputNumber placeholder={2} min={1} max={getMaxBy(metricName, unit)} />
+        </Item>
+      </Form>
+      <div className="goal-entry" />
     </Modal>
   );
 }
